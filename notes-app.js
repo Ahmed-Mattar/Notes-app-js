762e908dd43882c4e4b0471128f6eb169915c739
Ahmed Mattar <AceMattar@gmail.com>
2019-05-13 00:24:58 +0200

    feat: adding event listener

--- notes-app.js
+++ notes-app.js
@@ -1,1 +1,14 @@
-console.log('This is from a different file');
+const notes = [{
+    title: 'My next trip',
+    body:  'I would like to go to spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better'
+}, {
+    title:'Office modification',
+    body:'Get a new seat'
+}];
+
+document.querySelector('button').addEventListener('click',function(e) {
+    e.target.textContent = "The button was clicked";
+});
