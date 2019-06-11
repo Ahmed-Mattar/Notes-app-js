ad678e2880a6d3fba0fdbf32af59ad3f6945b43d
Ahmed Mattar <AceMattar@gmail.com>
2019-06-11 15:17:27 +0200

    feat: saving notes to local storage

--- notes-app.js
+++ notes-app.js
@@ -1,17 +1,17 @@
-const notes = [{
-    title: 'My next trip',
-    body:  'I would like to go to spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better'
-}, {
-    title:'Office modification',
-    body:'Get a new seat'
-}];
+let notes = [];
 
 const filters = {
     searchText: ''
 }
+
+// Check for existing saved data
+const notesJSON = localStorage.getItem('notes');
+
+if(notesJSON !== null){
+    notes = JSON.parse(notesJSON);
+}
+
+
 
 const renderNotes = function(notes,filters){
     const filteredNotes = notes.filter(function(note){
@@ -22,7 +22,14 @@ const renderNotes = function(notes,filters){
     
     filteredNotes.forEach(function(note){
         const noteEl = document.createElement('p');
-        noteEl.textContent = note.title;
+
+        if(note.title.length > 0){
+            noteEl.textContent = note.title;
+        } else {
+            noteEl.textContent = "Unnamed note";
+        }
+
+        
         document.querySelector('#notes').appendChild(noteEl);
     })
 };
@@ -30,16 +37,21 @@ const renderNotes = function(notes,filters){
 renderNotes(notes,filters);
 
 document.querySelector('#create-note').addEventListener('click',function(e) {
-    e.target.textContent = "The button was clicked";
+    notes.push({
+        title:'',
+        body:''
+    })
+    localStorage.setItem('notes',JSON.stringify(notes));
+    renderNotes(notes,filters);
 });
 
-document.querySelector('#remove-all').addEventListener('click', function(e){
-    document.querySelectorAll('.note').forEach(function(note){
-        note.remove();
-    });
-});
+
 
 document.querySelector('#search').addEventListener('input', function(e){
     filters.searchText = e.target.value;
     renderNotes(notes,filters);
+});
+
+document.querySelector('#filter-by').addEventListener('change',function(e){
+    console.log(e.target.value);
 });
