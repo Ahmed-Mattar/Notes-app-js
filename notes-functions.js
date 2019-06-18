e427f13c1280ffd52e8e4439918aa65b6e278f14
Ahmed Mattar <AceMattar@gmail.com>
2019-06-18 19:11:22 +0200

    feat:add an interface to a remove button

--- notes-functions.js
+++ notes-functions.js
@@ -1,7 +1,6 @@
 // Read existing notes from localStorage
 
 const getSavedNotes = function() {
-    // check for existing saved data
     // Check for existing saved data
     const notesJSON = localStorage.getItem('notes');
 
@@ -18,13 +17,23 @@ const saveNotes = function (notes){
 
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note){
-    const noteEl = document.createElement('p');
+    const noteEl = document.createElement('div');
+    const textEl = document.createElement('span');
+    const button = document.createElement('button');
 
+    // setup the remove note button
+    button.textContent = 'x';
+    noteEl.appendChild(button);
+
+    // setup the note title text
     if(note.title.length > 0){
-        noteEl.textContent = note.title;
+        textEl.textContent = note.title;
     } else {
-        noteEl.textContent = "Unnamed note";
+        textEl.textContent = "Unnamed note";
     }
+
+    noteEl.appendChild(textEl);
+    
 
     return noteEl;
 }
